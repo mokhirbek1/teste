0ae585fbc824ce1855d1c5c20cae7e1cb5f6d8de
cloverleaf08 <sabinax487@gmail.com>
2024-02-29 16:12:11 +0500

    finished project

---
+++ js/main.js
@@ -0,0 +1,30 @@
+let myForm = document.getElementById('myForm')
+let name = document.getElementById('name')
+let ship = document.getElementById('ship')
+let Phone = document.getElementById('phone')
+let contacts = document.getElementById('contacts')
+let contact__info = document.getElementById('contact__info')
+
+myForm.addEventListener('submit', function () {
+    let userName = name.value
+    let userShip = ship.value
+    let userPhone = Phone.value
+
+    let contact = document.createElement('div')
+    contact.innerHTML = `
+   <h1 class="title__name">${userName}</h1>
+   <h1 class="title__ship">${userShip}</h1>
+   <h1 class="title__number">${userPhone}</h1>
+   <button class="delete__btn">delete</button>`
+
+    contact.classList.add('contact__info')
+
+
+    contacts.append(contact)
+})
+
+contacts.addEventListener('click', function (event) {
+    if (event.target.classList.contains('delete__btn')) {
+        contacts.removeChild(event.target.parentNode)
+    }
+})
